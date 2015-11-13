de987c8f5cec28a3cf457117c8dda2801bd15d44
David Khourshid <davidkpiano@gmail.com>
2015-11-13 10:05:31 -0500

    Patronus stuff

--- wp-content/themes/custom/patronus/src/index.js
+++ wp-content/themes/custom/patronus/src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'isomorphic-fetch';
+
+function mapPatronus(patronus) {
+  return {
+    id: patronus.id,
+    ...patronus.acf
+  };
+}
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      patronuses: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://homepress.app/api/get_posts/?post_type=patronus')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          patronuses: res.posts.map(mapPatronus)
+        });
+      });
+  }
+
   render() {
     return (
-      <h1>Hello, world!</h1>
+      <div className="pt-app">
+        <div className="pt-field">
+          <input type="text" placeholder="Search for a Patronus..." id="" className="pt-input"/>
+        </div>
+        <ul className="pt-list">
+        { this.state.patronuses.map((patronus) => 
+          <li className="pt-item" key={patronus.id}>
+            <strong>{patronus.type}</strong> - {patronus.description}
+          </li>
+        )}
+        </ul>
+      </div>
     );
   }
 }
--- wp-content/themes/custom/patronus/webpack.config.js
+++ wp-content/themes/custom/patronus/webpack.config.js
@@ -34,7 +34,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin('main.css')
   ],
